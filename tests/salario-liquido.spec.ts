50d0f989e42357b0435b9fcf900f41643bca2e22
João Xavier <fastjonh@gmail.com>
2023-04-14 21:59:29 -0300

    typescript é paia pra setar as coisas em
    -desisti dos testes

--- tests/salario-liquido.spec.ts
+++
@@ -1,9 +0,0 @@
-import { salarioLiquido } from "../src/salarioLiquido";
-
-describe('testing calculadora salario liquido', () => {
-  test('4500', () => {
-    const salario = salarioLiquido(4500);
-    expect(salario.descontoTotal).toBe(729.92);
-    expect(salario.salarioLiquido).toBe(3770.08)
-  });
-});
